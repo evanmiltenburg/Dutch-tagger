77f7c82a52b26e66e8ba09b93090e882387fe00e
Emiel van Miltenburg <emielonline@gmail.com>
2016-06-12 15:59:37 +0200

    make extension explicit, see #1

--- create_models.sh
+++ create_models.sh
@@ -1,4 +1,4 @@
-cat large_tagger_model/tagger_model_part_a* > model.perc.dutch_tagger_large
+cat large_tagger_model/tagger_model_part_a* > model.perc.dutch_tagger_large.pickle
 rm -r large_tagger_model/
-cat small_tagger_model/tagger_model_part_a* > model.perc.dutch_tagger_small
+cat small_tagger_model/tagger_model_part_a* > model.perc.dutch_tagger_small.pickle
 rm -r small_tagger_model/
